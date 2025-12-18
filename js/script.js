bf7e2c8a7a35965f9b23e1041533a9b27186044a
Nehemia Hasbadana <nehemiahasbad594@gmail.com>
2025-12-18 11:39:22 +0700

    perubahan login

--- js/script.js
+++ js/script.js
@@ -16,3 +16,28 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
     }
 });
+
+const loginForm = document.getElementById("loginForm");
+const forgotForm = document.getElementById("forgotForm");
+
+function showForgot() {
+  loginForm.classList.add("hidden");
+  forgotForm.classList.remove("hidden");
+}
+
+function showLogin() {
+  forgotForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+}
+
+// Toggle password visibility
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  const type = passwordInput.type === "password" ? "text" : "password";
+  passwordInput.type = type;
+
+  togglePassword.classList.toggle("ri-eye-fill");
+  togglePassword.classList.toggle("ri-eye-off-fill");
+});
